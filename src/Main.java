a7f70a949f048dbe4d8d2fef54704e0082b7cf35
LandonTopy <landontopy@gmail.com>
2023-09-27 09:42:19 -0400

    Help

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int age;
+
+        System.out.println("How old are you?");
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+            if (age >= 21){
+                System.out.println("You get a wristband");
+            }
+
+        } else {
+            System.out.println("The value you entered is incorrect");
+        }
     }
 }
